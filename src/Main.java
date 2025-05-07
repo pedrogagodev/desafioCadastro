c9b29b59af75ecf4eaf19e5dea099a9fb5939f38
Pedro Oliveira <pedrogagodev@gmail.com>
2025-05-07 15:01:02 -0300

    feat: initialize ListAllPets method in Main.Java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
 import collector.PetCollector;
 import repository.PetRepository;
+import service.ListAllPets;
 import service.PrintMenu;
 import service.RegisterPet;
+import util.ReadFile;
+
 import java.util.Scanner;
 
 public class Main {
@@ -14,8 +17,13 @@ public class Main {
 
         RegisterPet registerService = new RegisterPet(petCollector, petRepository);
 
+        ReadFile readFile = new ReadFile("resources/registered-pets/20250506T16:11-NOTINFORMED.txt");
+
+        ListAllPets listAllPets = new ListAllPets(readFile);
+
         PrintMenu mainMenu = new PrintMenu();
         mainMenu.setRegisterService(registerService);
+        mainMenu.setListAllPetsService(listAllPets);
 
         mainMenu.printInitialMenu();
 
