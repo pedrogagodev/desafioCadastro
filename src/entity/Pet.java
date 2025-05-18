4def594cb58ea40f5aab4217d1f6ab3d2f37c3ef
Pedro Oliveira <pedrogagodev@gmail.com>
2025-05-18 17:07:27 -0300

    refactor: make changes in Pet entity

--- src/entity/Pet.java
+++ src/entity/Pet.java
@@ -2,86 +2,52 @@ package entity;
 
 import entity.enums.PetGender;
 import entity.enums.PetType;
-import exception.RequiredFieldException;
+
 
 public class Pet {
-    private String name;
-    private PetType petType; // required
-    private PetGender petGender; // required
-    private PetAddress petAddress; // required
-    private int petAge;
-    private double petWeight;
-    private String petBreed;
+    private final String name;
+    private final PetType petType;
+    private final PetGender petGender;
+    private final PetAddress petAddress;
+    private final int petAge;
+    private final double petWeight;
+    private final String petBreed;
+
+
+    Pet(String name, PetType petType, PetGender petGender, PetAddress petAddress, int petAge, double petWeight, String petBreed) {
+        this.name = name;
+        this.petType = petType;
+        this.petGender = petGender;
+        this.petAddress = petAddress;
+        this.petAge = petAge;
+        this.petWeight = petWeight;
+        this.petBreed = petBreed;
+    }
 
     public String getName() {
         return name;
-    }
-
-    public void setName(String name) {
-       if (name == null || name.isEmpty()) {
-           this.name = "NOT INFORMED";
-           return;
-       }
-
-        this.name = name;
     }
 
     public PetType getPetType() {
         return petType;
     }
 
-    public void setPetType(PetType petType) {
-        if (petType == null) {
-            throw new RequiredFieldException("petType is required, please provide a valid value.");
-        }
-        this.petType = petType;
-    }
-
     public PetGender getPetGender() {
         return petGender;
     }
 
-    public void setPetGender(PetGender petGender) {
-        if (petGender == null) {
-            throw new RequiredFieldException("petGender is required, please provide a valid value.");
-        }
-        this.petGender = petGender;
-    }
+    public PetAddress getPetAddress() { return petAddress; }
 
-    public PetAddress getPetAddress() {
-        return petAddress;
-    }
-
-    public void setPetAddress(PetAddress petAddress) {
-        if (petAddress == null) {
-            throw new RequiredFieldException("petAddress is required, please provide a valid value.");
-        }
-        this.petAddress = petAddress;
-    }
-
-    public int getPetAge() {
+    public Integer getPetAge() {
         return petAge;
     }
 
-    public void setPetAge(int petAge) {
-        this.petAge = petAge;
-    }
-
-    public double getPetWeight() {
+    public Double getPetWeight() {
         return petWeight;
     }
 
-    public void setPetWeight(double petWeight) {
-        this.petWeight = petWeight;
-    }
-
-
     public String getPetBreed() {
         return petBreed;
-    }
-
-    public void setPetBreed(String petBreed) {
-        this.petBreed = petBreed;
     }
 
 }
